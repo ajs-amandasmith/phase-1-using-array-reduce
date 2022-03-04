ad2d085b06b336b044d01cc45cf0f578abee6ebe
Amanda Smith <ajs.amandasmith@gmail.com>
2022-03-04 16:26:02 -0600

    commit tree 321450ca31a9f575d9c998603cbc55584350a80d

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+
+
+// // Code your solution here
+// const products = [
+//   {name: 'Shampoo', price: 4.99},
+//   {name: 'Donuts', price: 7.99},
+//   {name: 'Cookies', price: 6.49},
+//   {name: 'Bath Gel', price: 13.99}
+// ];
+
+// function getTotalAmountForProducts(products) {
+//   let totalPrice = 0;
+
+//   for (const product of products) {
+//     totalPrice += product.price;
+//   };
+
+//   return totalPrice;
+// }
+
+// console.log(getTotalAmountForProducts(products));
+
+
+// const couponLocations = [
+//   {room: 'Living room', amount: 5},
+//   {room: 'Kitchen', amount: 2},
+//   {room: 'Bathroom', amount: 1},
+//   {room: 'Master bedroom', amount: 7}
+// ];
+
+// function ourReduce(arr, reducer, init) {
+//   let accum = init;
+//   for (const element of arr) {
+//     accum = reducer(accum, element);
+//   };
+//   return accum;
+// }
+
+// function couponCounter(totalAmount, location) {
+//   return totalAmount + location.amount;
+// }
+
+// console.log(ourReduce(couponLocations, couponCounter, 0));
+// console.log(couponLocations.reduce(couponCounter, 0));
+
+// const doubleAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 10);
+
+// console.log(doubleAndSummed);
+
+// const showcases = {
+//   'Dance': [],
+//   'Visual': [],
+//   'Music': [],
+//   'Theater': [],
+//   'Writing': []
+// }
+
+// studentSorter.showcaseAssign(studentName);
+
+// incomingStudents.reduce(function(showcases, student) {showcases[studentSorter.showcaseAssign(student)].push(student)}, allShowcases);
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce(function(accumulator, element) { return element + accumulator}, 0);
+
+console.log(totalBatteries);
